ce241ed6310ab402bd681ba3672b33d5a4d47b91
satoshun00 <shun.sato@fenneclab.com>
2015-11-14 20:58:10 +0900

    chore(handler): サンプル解析結果

--- server/handler.js
+++ server/handler.js
@@ -14,9 +14,32 @@ router.get('/bears', (req, res) => {
   });
 });
 
-router.put('/pandas/:id', (req, res) => {
-  console.log(`pandId: ${req.params.id}`)
-  return res.sendStatus(204);
+router.get('/analyze', (req, res) => {
+  return res.status(200).json([{
+    id     : 1,
+    name   : 'さや',
+    age    : 24,
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/1.jpg',
+    matched: 80
+  }, {
+    id     : 2,
+    name   : 'しずか',
+    age    : 21,
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/2.jpg',
+    matched: 79
+  }, {
+    id     : 3,
+    name   : 'メロン',
+    age    : 20,
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/3.jpg',
+    matched: 76
+  }, {
+    id     : 4,
+    name   : 'みほ',
+    age    : 22,
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/4.jpg',
+    matched: 70
+  }]);
 });
 
 export default router;
