ef0d8239d54a3966f06a7bfd7c021dd120252539
satoshun00 <shun.sato@fenneclab.com>
2015-11-15 15:09:34 +0900

    fix(recommend): 微妙なバグを修正

--- lambda/util/recommend.js
+++ lambda/util/recommend.js
@@ -3,24 +3,67 @@ import rq from 'request-promise';
 
 const USERS = {
   '1': {
-    name : 'さや',
-    age  : 24,
-    image: 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/1.png'
+    name   : '佐々木 瞳',
+    age    : 26,
+    address: '港区',
+    job    : '保育士',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/1.png'
   },
   '2': {
-    name : 'しずか',
-    age  : 21,
-    image: 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/2.png'
+    name   : '山田 優',
+    age    : 24,
+    address: '杉並区',
+    job    : '会社員',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/2.png'
   },
   '3': {
-    name : 'メロン',
-    age  : 20,
-    image: 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/3.png'
+    name   : '山崎 愛',
+    age    : 26,
+    address: '目黒区',
+    job    : 'ミュージシャン',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/3.png'
   },
   '4': {
-    name : 'みほ',
-    age  : 22,
-    image: 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/4.png'
+    name   : '時田 みな',
+    age    : 22,
+    address: '新宿区',
+    job    : '学生',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/4.png'
+  },
+  '5': {
+    name   : '高田 まみ',
+    age    : 27,
+    address: '杉並区',
+    job    : 'フリーター',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/5.png'
+  },
+  '6': {
+    name   : '池田 百合',
+    age    : 30,
+    address: '千代田区',
+    job    : 'OL',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/6.png'
+  },
+  '7': {
+    name   : '篠田 ゆうこ',
+    age    : 22,
+    address: '横浜',
+    job    : '専門学生',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/7.png'
+  },
+  '8': {
+    name   : '笠井 枝理依',
+    age    : 28,
+    address: '新宿区',
+    job    : '会社員',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/8.png'
+  },
+  '9': {
+    name   : '横手 絵里子',
+    age    : 27,
+    address: '赤羽',
+    job    : '事務',
+    image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/9.png'
   }
 };
 
@@ -36,10 +79,14 @@ export default {
     return rq(options)
     .then(({result}) => {
       console.log(result);
-      return _.sortByOrder(result.map(e => {
+      return _.compact(_.sortByOrder(result.map(e => {
         const user = USERS[e.id];
+        if (!user) {
+          return null;
+        }
         user.matched = e.distance;
-      }), 'matched', 'desc');
+        return user;
+      }), 'matched', 'desc'));
     });
   }
 };
