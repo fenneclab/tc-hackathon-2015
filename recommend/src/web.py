5caba95a44d37aab46abc99973ddad9b79326fac
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2015-11-15 11:56:36 +0900

    リコメンドapi用のweb serverの仮response

--- recommend/src/web.py
+++ recommend/src/web.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 from flask import Flask, jsonify, request
@@ -10,25 +11,37 @@ app = Flask(__name__)
 # cross domain でアクセス可能にする
 CORS(app)
 
-# index にアクセスしたときの処理
-@app.route('/v1/', methods=['GET'])
-def index():
-    user_json = request['user']
+WORK_DIR = '/var/www'
 
+
+def example_response_json():
+    f = open(WORK_DIR + '/example_response.json', 'r')
+    results = f.read()
+    f.close()
+    return results
+
+
+def example_response():
     import json
-    response = json.load(example_response())
+    response = json.loads(example_response_json())
+    print response
     # result = {'result': }
     # return jsonify(result)
     return jsonify(response)
 
+
+# index にアクセスしたときの処理
+@app.route('/v1/', methods=['GET'])
+def index():
+    user_json = request.args.get('user', '')
+    print user_json
+
+    return example_response()
+
+    # result = {'result': }
+    # return jsonify(result)
+    # return jsonify(response)
+
 if __name__ == '__main__':
     app.debug = DEBUG
     app.run(host='0.0.0.0', port=PORT)
-
-
-
-def example_response():
-    f = open('example_response.json', 'r')
-    results = f.read()
-    f.close()
-    return results
