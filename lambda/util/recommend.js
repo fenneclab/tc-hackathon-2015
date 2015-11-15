0027204aaf4255fb96a656f00b5f0a2232620bf8
satoshun00 <shun.sato@fenneclab.com>
2015-11-15 15:44:48 +0900

    fix(recommend): バグとり

--- lambda/util/recommend.js
+++ lambda/util/recommend.js
@@ -2,63 +2,63 @@ import _ from 'lodash';
 import rq from 'request-promise';
 
 const USERS = {
-  '1': {
+  'akira': {
     name   : '佐々木 瞳',
     age    : 26,
     address: '港区',
     job    : '保育士',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/1.png'
   },
-  '2': {
+  'ami': {
     name   : '山田 優',
     age    : 24,
     address: '杉並区',
     job    : '会社員',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/2.png'
   },
-  '3': {
+  'ariana': {
     name   : '山崎 愛',
     age    : 26,
     address: '目黒区',
     job    : 'ミュージシャン',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/3.png'
   },
-  '4': {
+  'arison': {
     name   : '時田 みな',
     age    : 22,
     address: '新宿区',
     job    : '学生',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/4.png'
   },
-  '5': {
+  'gomaki': {
     name   : '高田 まみ',
     age    : 27,
     address: '杉並区',
     job    : 'フリーター',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/5.png'
   },
-  '6': {
+  'hirary': {
     name   : '池田 百合',
     age    : 30,
     address: '千代田区',
     job    : 'OL',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/6.png'
   },
-  '7': {
+  'kanako': {
     name   : '篠田 ゆうこ',
     age    : 22,
     address: '横浜',
     job    : '専門学生',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/7.png'
   },
-  '8': {
+  'love': {
     name   : '笠井 枝理依',
     age    : 28,
     address: '新宿区',
     job    : '会社員',
     image  : 'https://s3-ap-northeast-1.amazonaws.com/tc-hackathon-2015-kari/8.png'
   },
-  '9': {
+  'taylor': {
     name   : '横手 絵里子',
     age    : 27,
     address: '赤羽',
@@ -72,7 +72,7 @@ export default {
     const options = {
       url: 'http://52.192.17.71/recommend/v1',
       qs : {
-        user: persona
+        user: JSON.stringify(persona)
       },
       json: true
     };
@@ -86,7 +86,7 @@ export default {
         }
         user.matched = e.distance;
         return user;
-      }), 'matched', 'desc'));
+      }), 'matched', 'desc')).slice(0, 4);
     });
   }
 };
